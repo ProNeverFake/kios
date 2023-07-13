43cd121ecb8badfe7f770cbeb5bbc81362ed84d0
blackbird <jicong_ao@163.com>
2023-07-13 16:55:51 +0200

    backup 1307

---
+++ build/src/CMakeFiles/bt_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bt_node.dir/bt_node.cpp.o"
+  "CMakeFiles/bt_node.dir/bt_node.cpp.o.d"
+  "bt_node"
+  "bt_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bt_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/bt_node.cpp
+++ src/bt_node.cpp
@@ -19,9 +19,15 @@
 #include <string>
 #include <vector>
 
+#include <cinttypes>
+
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp/publisher_options.hpp"
 #include "std_msgs/msg/string.hpp"
+
+#include "example_interfaces/action/fibonacci.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+
 
 #include "bt_node.hpp"
 
@@ -110,16 +116,117 @@ void BehaviorTreeNode::print_network_flow_endpoints(
       stream.str().c_str());
 }
 
+using Fibonacci = example_interfaces::action::Fibonacci;
+// todo
+void feedback_callback(
+  rclcpp_action::ClientGoalHandle<Fibonacci>::SharedPtr,
+  const std::shared_ptr<const Fibonacci::Feedback> feedback)
+{
+  RCLCPP_INFO(
+    bt_node->get_logger(),
+    "Next number in sequence received: %" PRId32,
+    feedback->sequence.back());
+}
+
+
+
 int main(int argc, char *argv[])
 {
   rclcpp::init(argc, argv);
 
+  bt_node = rclcpp::Node::make_shared("behavior_tree_node");
+
+  auto action_client = rclcpp_action::create_client<Fibonacci>(bt_node, "bt_action");
+
+  // find action server
+  if (!action_client->wait_for_action_server(std::chrono::seconds(20))) {
+    RCLCPP_ERROR(bt_node->get_logger(), "Action server not available after waiting");
+    return 1;
+  }
+
+  // generate a goal msg
+  auto goal_msg = Fibonacci::Goal();
+  goal_msg.order = 10;
   
+  RCLCPP_INFO(bt_node->get_logger(), "Sending goal");
 
+  // set options for sending the goal
+  auto send_goal_options = rclcpp_action::Client<Fibonacci>::SendGoalOptions();
+  send_goal_options.feedback_callback = feedback_callback;
+  // generate the future handle used to spin
+  auto goal_handle_future = action_client->async_send_goal(goal_msg, send_goal_options);
 
+  // spin the node with the goal handle and get the completed future
+  if (rclcpp::spin_until_future_complete(bt_node, goal_handle_future) !=
+    rclcpp::FutureReturnCode::SUCCESS)
+  {
+    RCLCPP_ERROR(bt_node->get_logger(), "send goal call failed :(");
+    return 1;
+  }
 
+  // get the handle in spin
+  rclcpp_action::ClientGoalHandle<Fibonacci>::SharedPtr goal_handle = goal_handle_future.get();
+  
+  if (!goal_handle) { //* if the shareptr is null or empty
+    RCLCPP_ERROR(bt_node->get_logger(), "Goal was rejected by server");
+    return 1;
+  }
 
-  rclcpp::spin(std::make_shared<BehaviorTreeNode>());
+  // get the result handle w.r.t the goal handle
+  auto result_future = action_client->async_get_result(goal_handle);
+
+  RCLCPP_INFO(bt_node->get_logger(), "Waiting for result");
+
+  // spin to ask for result with result handle
+  if (rclcpp::spin_until_future_complete(bt_node, result_future) !=
+    rclcpp::FutureReturnCode::SUCCESS)
+  {
+    RCLCPP_ERROR(bt_node->get_logger(), "get result call failed :(");
+    return 1;
+  }
+
+  // get the result
+  rclcpp_action::ClientGoalHandle<Fibonacci>::WrappedResult wrapped_result = result_future.get();
+
+  // check the result code
+  switch (wrapped_result.code) {
+    case rclcpp_action::ResultCode::SUCCEEDED:
+      break;
+    case rclcpp_action::ResultCode::ABORTED:
+      RCLCPP_ERROR(bt_node->get_logger(), "Goal was aborted");
+      return 1;
+    case rclcpp_action::ResultCode::CANCELED:
+      RCLCPP_ERROR(bt_node->get_logger(), "Goal was canceled");
+      return 1;
+    default:
+      RCLCPP_ERROR(bt_node->get_logger(), "Unknown result code");
+      return 1;
+  }
+
+  RCLCPP_INFO(bt_node->get_logger(), "result received");
+
+  // todo
+  for (auto number : wrapped_result.result->sequence) {
+    RCLCPP_INFO(bt_node->get_logger(), "%" PRId32, number);
+  }
+
+  // reset the client and the node
+  action_client.reset();
+  bt_node.reset();
+
+  // rclcpp::spin(std::make_shared<BehaviorTreeNode>());
   rclcpp::shutdown();
   return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
